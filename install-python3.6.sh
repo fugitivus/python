6a746ef5c9f4a79d1d1bb68dd43749213d111af2
fugitivus <fugitivus@users.noreply.github.com>
2020-02-24 09:47:32 +0100

    Delete install-python3.6.sh

--- install-python3.6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
-tar xvf Python-3.6.9.tgz
-cd Python-3.6.9
-./configure --enable-optimizations --enable-shared
-make -j8
-sudo make altinstall
-python3.6
